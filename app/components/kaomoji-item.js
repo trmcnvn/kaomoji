133ea37b642ded147f5e35395af54f914e6e0bb8
Thomas McNiven <hello@vevix.net>
2016-07-10 16:32:31 +1000

    Add notifications on copy events

--- app/components/kaomoji-item.js
+++ app/components/kaomoji-item.js
@@ -1,9 +1,30 @@
 import Ember from 'ember';
+/* global noty */
 
 const {
   Component
 } = Ember;
 
 export default Component.extend({
-  classNames: ['kaomoji-item']
+  classNames: ['kaomoji-item'],
+
+  _createNoty(text) {
+    noty({
+      layout: 'bottom',
+      theme: 'relax',
+      type: 'success',
+      timeout: 1000,
+      text
+    });
+  },
+
+  actions: {
+    success(event) {
+      this._createNoty(`${event.text} -- copied to clipboard!`);
+    },
+
+    error() {
+      this._createNoty('Failed to copy kaomoji to the clipboard (′︿‵｡)');
+    }
+  }
 });
